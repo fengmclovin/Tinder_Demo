c5f408e17407162cd9cf4b95a0a3c742530751b6
Lance Tian <79405882+fengmclovin@users.noreply.github.com>
2024-04-17 23:45:52 +1000

    Create unitest_test.py

---
+++ Test_Code/unitest_test.py
@@ -0,0 +1,27 @@
+import unittest
+
+from prime import is_prime
+
+
+class Tests(unittest.TestCase):
+    def test_1(self):
+        self.assertFalse(is_prime(1))
+
+    def test_2(self):
+        self.assertTrue(is_prime(2))
+
+    def test_8(self):
+        self.assertFalse(is_prime(8))
+
+    def test_11(self):
+        self.assertTrue(is_prime(11))
+
+    def test_25(self):
+        self.assertFalse(is_prime(25))
+
+    def test_28(self):
+        self.assertFalse(is_prime(28))
+
+
+if __name__ == "__main__":
+    unittest.main()
